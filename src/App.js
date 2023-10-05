50aa101f69024ac192a0779558f52e454a2e926c
Riddhi1622 <90517553+Riddhi1622@users.noreply.github.com>
2023-10-04 23:22:19 -0400

    Added Background

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> Hello Professor.
+      This is assignment by Divya R. Bhamerkar
+      My nickname is Riddhi!!
+      My student id is:-  101474219
+    </h1>
     </div>
   );
 }
